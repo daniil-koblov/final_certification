f36fc65ef2764e614c21350163aba20b1487450d
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2024-10-01 19:48:35 +0300

    delete PrescriptionCategory app in precription_website/settings.py and  precription_website/

--- prescription_website/prescription_website/urls.py
+++ prescription_website/prescription_website/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('PrescriptionApp.urls')),
-    path('category/', include('PrescriptionCategory.urls')),
 ]
